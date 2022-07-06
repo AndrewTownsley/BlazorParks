5bc305e981a6c2bc68a41eb36bf7a88a73260e2a
Andrew TOwnsley <atownsley86@gmail.com>
2022-07-06 09:57:04 -0600

    Park List created and rendering parks.  Need to create add Park form, and figure out how to pass LIST from page to page.

---
+++ ParkTracker_Server/Models/Park.cs
@@ -0,0 +1,16 @@
+﻿namespace ParkTracker_Server.Models
+{
+    public class Park
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string State { get; set; }
+
+        public string Description { get; set; }
+
+        public string Activities { get; set; }
+
+        public bool Fee { get; set; }
+    }
+}
